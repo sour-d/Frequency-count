1c77f343b9be3dacd59ff0abecf7b32a242d0f32
Sourav das <36912347+sour-d@users.noreply.github.com>
2022-04-05 20:41:06 +0530

    implemented frequency count using library

--- frequencyCount.js
+++ frequencyCount.js
@@ -1,57 +1,16 @@
-const isArray = function (content) {
-  return Array.isArray(content);
-};
+const arrayMethods = require('./library.js');
 
-const isEqual = function (content1, content2) {
-  if (content1 === content2) {
-    return true;
-  }
-  if (isArray(content1) && isArray(content2)) {
-    return areArraysEqual(content1, content2);
-  }
-  return false;
-};
-
-const areArraysEqual = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let index = 0; index < array1.length; index++) {
-    if (!isEqual(array1[index], array2[index])) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const positionOf = function (element, groups) {
-  for (let index = 0; index < groups.length; index++) {
-    if (isEqual(element, groups[index][0])) {
-      return index;
-    }
-  }
-  return -1;
-};
-
-const countOccurence = function (element, frequencyCounts) {
-  let position = positionOf(element, frequencyCounts);
+const addCount = function (element, position, groups) {
   if (position === -1) {
-    frequencyCounts.push([]);
-    position = frequencyCounts.length - 1;
-    frequencyCounts[position].push(element);
-    frequencyCounts[position].push(0);
+    position = groups.length;
+    groups.push([element, 0]);
   }
-  count = frequencyCounts[position][1];
-  frequencyCounts[position][1] = count + 1;
-  return frequencyCounts;
+  groups[position][1]++;
+  return groups;
 };
 
-const countFrequency = function (elements) {
-  if (elements.length === 0) {
-    return [];
-  }
-  const frequencyCounts = countFrequency(elements.slice(1));
-  return countOccurence(elements[0], frequencyCounts);
+const countFrequency = function (set) {
+  return arrayMethods.group(set, addCount);
 };
 
 console.log(countFrequency([1, 2, 3]));
